cecce4991f814ea0c3d5a8ca3715b7412f824264
kavi khalique <120750626+kavikhalique@users.noreply.github.com>
2022-12-24 12:20:12 +0530

    update

--- app/src/main/java/com/kavi_khalique/covidtracker/MainActivity.kt
+++ app/src/main/java/com/kavi_khalique/covidtracker/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.kavi_khalique.covidtracker
 
+// this app is under construction
+// till now i am focusing on graphics/UI only 
+// after completing UI i will write codes to create all the buttons working
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
